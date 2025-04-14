c1fd88c03737e2ee565e11a750b7194407f1ad2b
drf0rk <wolfparade@hotmail.com>
2025-04-14 12:02:56 +1200

    Update default.sh

--- provisioning_scripts/default.sh
+++ provisioning_scripts/default.sh
@@ -17,30 +17,25 @@ EXT_DIR="$A1111_DIR/extensions"
 
 # List of extensions to clone (ensure Mikubill/sd-webui-controlnet is included if needed by other extensions, otherwise omit)
 EXTENSIONS=(
-  "https://github.com/Mikubill/sd-webui-controlnet.git" # Keeping this as it's often required by other CN-related extensions
-  #"https://github.com/fkunn1326/openpose-editor.git"
-  #"https://github.com/camenduru/stable-diffusion-webui-huggingface.git"
-  #"https://github.com/camenduru/stable-diffusion-webui-tunnels.git"
-  #"https://github.com/etherealxx/batchlinks-webui.git"
-  #"https://github.com/camenduru/stable-diffusion-webui-catppuccin.git"
-  #"https://github.com/KohakuBlueleaf/a1111-sd-webui-locon.git"
-  "https://github.com/AUTOMATIC1111/stable-diffusion-webui-rembg.git"
-  #"https://github.com/ashen-sensored/stable-diffusion-webui-two-shot.git"
-  #"https://github.com/thomasasfk/sd-webui-aspect-ratio-helper.git"
-  #"https://github.com/tjm35/asymmetric-tiling-sd-webui.git"
-  #"https://github.com/pkuliyi2015/multidiffusion-upscaler-for-automatic1111.git"
-  #"https://github.com/Coyote-A/ultimate-upscale-for-automatic1111.git"
-  #"https://github.com/kohya-ss/sd-webui-additional-networks.git"
-  #"https://github.com/AlUlkesh/stable-diffusion-webui-images-browser.git"
-  "https://github.com/continue-revolution/sd-webui-segment-anything.git"
-  "https://github.com/civitai/sd_civitai_extension.git"
-  #"https://github.com/Gourieff/sd-webui-reactor.git"
-  #"https://github.com/hnmr293/posex.git"
-  #"https://github.com/nonnonstop/sd-webui-3d-open-pose-editor.git"
-  "https://github.com/v8hid/infinite-image-Browse.git"
-  #"https://github.com/toshiaki1729/stable-diffusion-webui-dataset-tag-editor.git"
-  #"https://github.com/toriato/stable-diffusion-webui-wd14-tagger.git"
-  "https://github.com/adieyal/sd-dynamic-prompts.git"
+
+EXTENSIONS_TO_INSTALL=(
+    "https://github.com/AUTOMATIC1111/stable-diffusion-webui-promptgen"
+    "https://github.com/continue-revolution/sd-webui-segment-anything"
+    "https://github.com/modelscope/facechain"
+    "https://github.com/glucauze/sd-webui-faceswaplab"
+    "https://github.com/cheald/sd-webui-loractl"
+    "https://github.com/light-and-ray/sd-webui-replacer"
+    "https://github.com/gutris1/sd-hub"
+    "https://github.com/Avaray/lora-keywords-finder"
+    "https://github.com/kainatquaderee/sd-webui-reactor-Nsfw_freedom"
+    "https://github.com/Haoming02/sd-webui-mosaic-outpaint"
+    "https://github.com/zero01101/openOutpaint-webUI-extension"
+    "https://github.com/Mikubill/sd-webui-controlnet" # Keep extension install
+    "https://github.com/Coyote-A/ultimate-upscale-for-automatic1111"
+    "https://github.com/zanllp/sd-webui-infinite-image-browsing"
+    "https://github.com/alemelis/sd-webui-ar"
+    "https://github.com/Uminosachi/sd-webui-inpaint-anything"
+
 )
 
 # SDXL Model URL and Filename
